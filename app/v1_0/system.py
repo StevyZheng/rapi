1ba4052763124f6727aea41051e233cda14e2b8e
664553716@qq.com <jqy951753>
2018-12-26 22:31:38 +0800

    init 1

--- app/v1_0/system.py
+++ app/v1_0/system.py
@@ -3,6 +3,7 @@ from app import db
 from flask_restplus import Api, Resource
 from flask import jsonify, request
 from app.v1_0 import v1_0
+from libs.system import Common
 
 api_system = Api(v1_0)
 
@@ -10,4 +11,7 @@ api_system = Api(v1_0)
 @api_system.route('/sysinfo')
 class Test(Resource):
     def get(self):
-        pass
+        tmp = Common.shell_exec("cat /etc/redhat-release").strip()
+        return jsonify({
+            "os": tmp
+        })
